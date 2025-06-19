77077ad257d88c39b2b8c65cee3efc3e741b7388
Levi Handke <l.handke@addon-solution.de>
2025-06-19 17:17:35 +0200

    feat: update filter for role check

--- frontend/src/pages/Batches.vue
+++ frontend/src/pages/Batches.vue
@@ -198,12 +198,12 @@ const updateTabFilter = () => {
 	if (!user.data) {
 		return
 	}
-	if (currentTab.value == 'Enrolled' && (is_student.value||user.data.is_instructor)) {
+	if (currentTab.value == 'Enrolled' && (is_student.value||user.data.roles.includes('Course Creator'))) {
 		filters.value['enrolled'] = 1
 		delete filters.value['start_date']
 		delete filters.value['published']
 		orderBy.value = 'start_date desc'
-	} else if (is_student.value||user.data.is_instructor) {
+	} else if (is_student.value||user.data.roles.includes('Course Creator')) {
 		delete filters.value['enrolled']
 	} else {
 		delete filters.value['start_date']
